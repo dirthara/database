b21a5c1fd2f4317e336202e1f9fe11abd6b06ec1
bricknpc <bricknpc@proton.me>
2026-09-07 07:48:39 +0200

    Check mysql error code for the exception

--- src/Connection/Exceptions/QueryException.php
+++ src/Connection/Exceptions/QueryException.php
@@ -7,12 +7,25 @@ namespace Dirthara\Database\Connection\Exceptions;
 use PDOException;
 use Dirthara\Database\Exceptions\DatabaseException;
 
+use function is_int;
+use function is_string;
+
 class QueryException extends DatabaseException
 {
     public static function fromPdo(PDOException $exception, string $query): self
     {
-        return new self(message: $exception->getMessage(), code: $exception->getCode(), previous: $exception, context: [
-            'query' => $query,
-        ]);
+        $code = $exception->getCode();
+        $context = ['query' => $query];
+
+        if (is_string($code)) {
+            $context['sqlstate'] = $code;
+        }
+
+        return new self(
+            message: $exception->getMessage(),
+            code: is_int($code) ? $code : 0,
+            previous: $exception,
+            context: $context,
+        );
     }
 }
